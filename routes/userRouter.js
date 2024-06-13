11cd06643e88d40b74449cd1075882e7dd30cc07
Deborah Okeke <okekedeborah@gmail.com>
2024-06-13 22:06:26 +0100

    feat: implement viewing of user profile

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,9 +3,12 @@ const userRouter = express.Router()
 const createUser = require('../controllers/userControllers/signupController')
 const { verifyMail, sendConfirmationMail } = require('../controllers/userControllers/verifyMailController')
 const userLogin = require('../controllers/userControllers/loginController')
+const { authenticate } = require('../middleware/authentication')
+const getUser = require('../controllers/userControllers/viewProfileController')
 
 userRouter.post('/register', createUser)
 userRouter.get('/verify', verifyMail, sendConfirmationMail)
 userRouter.post('/login', userLogin)
+userRouter.get('/user', authenticate, getUser)
 
 module.exports = userRouter
