74ae49bfac48ecbdac9416081e968f93d368c013
Deborah Okeke <okekedeborah@gmail.com>
2024-06-13 21:39:03 +0100

    feat: implement authentication middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
+async function authenticate (req, res, next) {
+	try {
+		const token = req.headers.authorization
+		if (!token) {
+			return res.status(401).json({success: false, message: 'Unauthorized. Please login'})
+		}
+		
+		try {
+			const decoded = jwt.verify(token, process.env.SECRET)
+			req.user = decoded
+			next()
+
+		} catch (error) {
+			console.error(error.message)
+			return res.status(401).json({success: false, message: error.message})
+		}
+	} catch (error) {
+		console.error(error.message)
+		res.status(500).json({success: false, message: 'Internal server error'})
+	}
+}
+
+module.exports = {authenticate}
