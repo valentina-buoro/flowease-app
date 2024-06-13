ca724607d00d590ad0c5b3e8f6fc71e2aea69c11
Deborah Okeke <okekedeborah@gmail.com>
2024-06-13 20:17:58 +0100

    feat: implement user login

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,8 +2,10 @@ const express = require('express')
 const userRouter = express.Router()
 const createUser = require('../controllers/userControllers/signupController')
 const { verifyMail, sendConfirmationMail } = require('../controllers/userControllers/verifyMailController')
+const userLogin = require('../controllers/userControllers/loginController')
 
 userRouter.post('/register', createUser)
 userRouter.get('/verify', verifyMail, sendConfirmationMail)
+userRouter.post('/login', userLogin)
 
 module.exports = userRouter
