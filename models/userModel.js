fe59f8020556c5ad6524386de514312f9a7d8d8c
Deborah Okeke <okekedeborah@gmail.com>
2024-06-13 19:01:54 +0100

    feat: create user model

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        full_name: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        verified: {
+            type: Boolean,
+            default: false
+        },
+        created_projects: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Project'
+        }],
+        assigned_projects: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Project'
+        }],
+        profile_picture: {
+            type: String
+        }
+    }
+)
+
+userSchema.pre('save', async function (next) {
+	const user = this
+	try {
+		if (user.isModified('password')) {
+			const hashedPassword = await bcrypt.hash(user.password, 10)
+			user.password = hashedPassword
+		}
+		next()
+	} catch (error) {
+		return next(error)
+	}
+})
+
+module.exports = mongoose.model('User', userSchema)
