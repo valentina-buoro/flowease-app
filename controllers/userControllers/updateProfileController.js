7435cb93d66b2b6b57103bfca6e45f0123d750da
Deborah Okeke <okekedeborah@gmail.com>
2024-06-14 11:07:26 +0100

    feat: implement update of user profile and upload of profile picture

---
+++ controllers/userControllers/updateProfileController.js
@@ -0,0 +1,31 @@
+const UserModel = require('../../models/userModel');
+
+async function updateUser(req, res) {
+    try {
+        const { email, full_name, password } = req.body
+        
+        const user_id = req.user.id
+        const user = await UserModel.findById(user_id).select('-password -__v')
+        if (!user) {
+            return res.status(404).json({success: false, message: 'User not found'})
+        }
+  
+      if (email) user.email = email
+      if (full_name) user.full_name = full_name
+      if (password) user.password = password
+      if (req.file) {
+        const profile_picture_url = req.file.path
+        user.profile_picture = profile_picture_url
+      }
+  
+      const updatedUser = await user.save()
+  
+      res.status(200).json({success: true, message: updatedUser})
+      
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({success: false, message: 'Internal server error'})
+    }
+}
+  
+module.exports = updateUser;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,11 +6,14 @@ const userLogin = require('../controllers/userControllers/loginController')
 const { authenticate } = require('../middleware/authentication')
 const getUser = require('../controllers/userControllers/viewProfileController')
 const getUserById = require('../controllers/userControllers/viewUserController')
+const updateUser = require('../controllers/userControllers/updateProfileController')
+const upload = require('../utils/imageUpload')
 
 userRouter.post('/register', createUser)
 userRouter.get('/verify', verifyMail, sendConfirmationMail)
 userRouter.post('/login', userLogin)
 userRouter.get('/user', authenticate, getUser)
 userRouter.get('/:user_id/user', getUserById)
+userRouter.put('/user/edit', authenticate, upload.single('profile_picture'), updateUser)
 
 module.exports = userRouter
