dd1910416cae4745421d80a2ed18e28ccb463e65
Deborah Okeke <okekedeborah@gmail.com>
2024-06-14 21:00:32 +0100

    feat: implement listing of projects

---
+++ controllers/projectControllers/listProjectsController.js
@@ -0,0 +1,40 @@
+const UserModel = require("../../models/userModel");
+
+async function listProjects(req, res) {
+  try {
+    const user_id = req.user.id;
+    const user = await UserModel.findById(user_id)
+        .populate({
+            path: 'created_projects',
+            populate: {
+                path: 'milestones'
+            }
+    })
+        .populate({
+            path: 'assigned_projects',
+            populate: {
+                path: 'milestones'
+            }
+    })
+    .exec()
+    
+    if (!user) {
+        return res.status(404).json({success: false, message: 'User not found'})
+      }
+      
+      const projects = {
+          created_projects: user.created_projects,
+          assigned_projects: user.assigned_projects
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: projects });
+  } catch (error) {
+    console.log(error.message);
+   
+    res.status(500).json("Internal server error");
+  }
+}
+
+module.exports = listProjects;
--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -2,9 +2,11 @@ const express = require("express");
 const createProject = require("../controllers/projectControllers/createProjectController");
 const { authenticate } = require("../middleware/authentication");
 const getProjectById = require("../controllers/projectControllers/viewProjectController");
+const listProjects = require("../controllers/projectControllers/listProjectsController");
 const projectRouter = express.Router();
 
 projectRouter.post("/create", authenticate, createProject);
 projectRouter.get("/:project_id/project", getProjectById);
+projectRouter.get("/list", authenticate, listProjects);
 
 module.exports = projectRouter;
