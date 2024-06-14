4f676d2802feb59fef3bd8c54c47c24058dd4b4e
Deborah Okeke <okekedeborah@gmail.com>
2024-06-14 22:09:12 +0100

    feat: add route for updating project

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -3,10 +3,13 @@ const createProject = require("../controllers/projectControllers/createProjectCo
 const { authenticate } = require("../middleware/authentication");
 const getProjectById = require("../controllers/projectControllers/viewProjectController");
 const listProjects = require("../controllers/projectControllers/listProjectsController");
+const updateProject = require("../controllers/projectControllers/updateProjectController");
+
 const projectRouter = express.Router();
 
 projectRouter.post("/create", authenticate, createProject);
 projectRouter.get("/:project_id/project", getProjectById);
 projectRouter.get("/list", authenticate, listProjects);
+projectRouter.put("/:project_id/edit", authenticate, updateProject);
 
 module.exports = projectRouter;
