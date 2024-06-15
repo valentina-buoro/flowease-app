62602724bdd438cddded9d768e6e776ed4c8fcd8
valentina buoro <valentinabuoro4@gmail.com>
2024-06-15 23:54:37 +0100

    landing page features

--- src/components/landingpageComponents/contact.jsx
+++ src/components/landingpageComponents/contact.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import dashboard from "../../assets/images/landingpageImages/dashboardPicture.png";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="grid grid-cols-12 gap-4 mx-auto mt-10">
+        <div className="col-span-7 flex flex-col items-center justify-center">
+          <div className=" w-[494px]">
+          <p className="font-bold text-5xl">
+            Start Optimizing Your Performance <span className="text-primaryBlue">Today!</span>
+          </p>
+          <button className="bg-primaryBlue  text-[#ffff] rounded-2xl py-2.5 px-7  font-bold">
+            Create Project
+          </button>
+          </div>
+        </div>
+        <div className="col-span-5">
+          <img src={dashboard} alt="dashboard" className="w-full" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
