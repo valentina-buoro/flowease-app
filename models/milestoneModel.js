b3ef4f8a871b76f6c370b6569f548a5b6cf4fa6c
Deborah Okeke <okekedeborah@gmail.com>
2024-06-15 09:52:24 +0100

    feat: create milestone model

---
+++ models/milestoneModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const milestoneSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    due_date: {
+        type: Date,
+        required: true
+    },
+    project_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+        required: true
+    },
+    collaborator: {
+        type: String
+    },
+    started: {
+        type: Boolean,
+        default: false
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = mongoose.model('Milestone', milestoneSchema)
