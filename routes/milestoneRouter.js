f738c5b3c6bec6e7457677a183331d80316fd449
Deborah Okeke <okekedeborah@gmail.com>
2024-06-15 21:01:12 +0100

    feat: add route to mark milestone as complete

--- routes/milestoneRouter.js
+++ routes/milestoneRouter.js
@@ -2,9 +2,11 @@ const express = require('express')
 const createMilestone = require('../controllers/milestoneControllers/createMilestoneController')
 const { authenticate } = require('../middleware/authentication')
 const markMilestoneStarted = require('../controllers/milestoneControllers/milestoneStartController')
+const markMilestoneCompleted = require('../controllers/milestoneControllers/milestoneCompleteController')
 const milestoneRouter = express.Router()
 
 milestoneRouter.post('/:project_id/create', authenticate, createMilestone)
 milestoneRouter.put('/:milestone_id/started', authenticate, markMilestoneStarted)
+milestoneRouter.put('/:milestone_id/completed', authenticate, markMilestoneCompleted)
 
 module.exports = milestoneRouter
