02bd3942088ec9b94836c7babe5be65366c50220
Deborah Okeke <okekedeborah@gmail.com>
2024-06-15 18:49:10 +0100

    feat: add route to mark milestone as 'started'

--- routes/milestoneRouter.js
+++ routes/milestoneRouter.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const createMilestone = require('../controllers/milestoneControllers/createMilestoneController')
 const { authenticate } = require('../middleware/authentication')
+const markMilestoneStarted = require('../controllers/milestoneControllers/milestoneStartController')
 const milestoneRouter = express.Router()
 
 milestoneRouter.post('/:project_id/create', authenticate, createMilestone)
+milestoneRouter.put('/:milestone_id/started', authenticate, markMilestoneStarted)
 
 module.exports = milestoneRouter
