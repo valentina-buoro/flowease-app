529b5e9a653e55e98cbeabf85b2b0d8113d9c7e2
valentina buoro <valentinabuoro4@gmail.com>
2024-06-15 19:44:42 +0100

    feat: dashboard components

---
+++ src/components/dashboardComponent/dashboardComponent.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import CreateProject from './childrenComponents/createProject'
+import ProjectStats from './childrenComponents/projectstats'
+import Productivity from './childrenComponents/productivity'
+import DashboardClock from './childrenComponents/clock'
+
+const DashboardComponent = ({data}) => {
+  return (
+    <div className='px-4 md:px-11  w-full'>
+        <CreateProject/>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-y-10 gap-x-6'>
+            <div className='col-span-1 md:col-span-2 '>
+            <ProjectStats data={data} />
+            <DashboardClock/>
+            </div>
+            
+            <Productivity />
+        </div>
+    </div>
+  )
+}
+
+export default DashboardComponent
