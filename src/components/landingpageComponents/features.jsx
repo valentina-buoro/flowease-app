71a364287c1e7f13010ca7a4c3fde01f1937e579
valentina buoro <valentinabuoro4@gmail.com>
2024-06-15 22:19:03 +0100

    feature cards

--- src/components/landingpageComponents/features.jsx
+++ src/components/landingpageComponents/features.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
+import React from "react";
+import featuresOne from "../../assets/svgs/landingpageSvg/featuresOne.svg";
 
 const Features = () => {
   return (
-    <div className='bg-[#F3F6FF]'>
-      <div className='text-center'>
-        <p className='text-[#1E1F24] font-bold text-5xl'>Features</p>
-        <p className='text-[#8E8E93] font-normal text-2xl'>Streamline Your Workflow and Boost Productivity</p>
+    <div className="bg-[#F3F6FF]">
+      <div className="text-center  py-10">
+        <p className="text-[#1E1F24] font-bold text-5xl">Features</p>
+        <p className="text-[#8E8E93] font-normal text-2xl">
+          Streamline Your Workflow and Boost Productivity
+        </p>
       </div>
-</div>
-  )
-}
+      <div className="max-w-[887px] grid grid-cols-2 gap-4 mx-auto mt-10">
+        <div className="col-span-1 ">
+          <div className="flex flex-col gap-5 h-[626px]">
+          <div className="bg-[#FFFFFF] rounded-2xl p-10 w-11/12 ">
+            <div className="flex justify-center">
+              <img src={featuresOne} alt="features-one" />
+            </div>
+            <p className="text-center font-bold text-[#1E1F24] py-4">Track Individual and Team Performance</p>
+            <p className="font-normal text-sm text-[#444750] text-center">Monitor progress, completed tasks, and upcoming milestones in one place</p>
+          </div>
+          <div className="bg-[#FFFFFF] rounded-2xl p-10 ml-auto w-11/12 ">
+            <div className="flex justify-center">
+              <img src={featuresOne} alt="features-one" />
+            </div>
+            <p className="text-center font-bold text-[#1E1F24] py-4">Track Individual and Team Performance</p>
+            <p className="font-normal text-sm text-[#444750] text-center">Monitor progress, completed tasks, and upcoming milestones in one place</p>
+          </div>
+          </div>
+        </div>
+        <div className="col-span-1 ">
+          <div className="flex flex-col mt-5 gap-5 h-[626px]">
+          <div className="bg-[#FFFFFF] rounded-2xl p-10 w-11/12 ">
+            <div className="flex justify-center">
+              <img src={featuresOne} alt="features-one" />
+            </div>
+            <p className="text-center font-bold text-[#1E1F24] py-4">Track Individual and Team Performance</p>
+            <p className="font-normal text-sm text-[#444750] text-center">Monitor progress, completed tasks, and upcoming milestones in one place</p>
+          </div>
+          <div className="bg-[#FFFFFF] rounded-2xl p-10 ml-auto w-11/12">
+            <div className="flex justify-center">
+              <img src={featuresOne} alt="features-one" />
+            </div>
+            <p className="text-center font-bold text-[#1E1F24] py-4">Track Individual and Team Performance</p>
+            <p className="font-normal text-sm text-[#444750] text-center">Monitor progress, completed tasks, and upcoming milestones in one place</p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Features
+export default Features;
