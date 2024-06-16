9a97ecc4f3ffaab74d4f9f39694c4ccbb6989510
Deborah Okeke <okekedeborah@gmail.com>
2024-06-16 14:19:27 +0100

    feat: add route to get project progress status

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -4,6 +4,7 @@ const { authenticate } = require("../middleware/authentication");
 const getProjectById = require("../controllers/projectControllers/viewProjectController");
 const listProjects = require("../controllers/projectControllers/listProjectsController");
 const updateProject = require("../controllers/projectControllers/updateProjectController");
+const getProjectProgressStatus = require("../controllers/projectControllers/getProjectProgressStatusController");
 
 const projectRouter = express.Router();
 
@@ -11,5 +12,6 @@ projectRouter.post("/create", authenticate, createProject);
 projectRouter.get("/:project_id/project", getProjectById);
 projectRouter.get("/list", authenticate, listProjects);
 projectRouter.put("/:project_id/edit", authenticate, updateProject);
+projectRouter.get('/:project_id/status', getProjectProgressStatus)
 
 module.exports = projectRouter;
