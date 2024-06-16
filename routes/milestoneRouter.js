2c758c034762d3aa14965917c1df226bd8b5e84a
Deborah Okeke <okekedeborah@gmail.com>
2024-06-16 16:42:13 +0100

    feat: add route to view milestone

--- routes/milestoneRouter.js
+++ routes/milestoneRouter.js
@@ -3,10 +3,12 @@ const createMilestone = require('../controllers/milestoneControllers/createMiles
 const { authenticate } = require('../middleware/authentication')
 const markMilestoneStarted = require('../controllers/milestoneControllers/milestoneStartController')
 const markMilestoneCompleted = require('../controllers/milestoneControllers/milestoneCompleteController')
+const getMilestoneById = require('../controllers/milestoneControllers/viewMilestoneController')
 const milestoneRouter = express.Router()
 
 milestoneRouter.post('/:project_id/create', authenticate, createMilestone)
 milestoneRouter.put('/:milestone_id/started', authenticate, markMilestoneStarted)
 milestoneRouter.put('/:milestone_id/completed', authenticate, markMilestoneCompleted)
+milestoneRouter.get('/:milestone_id/milestone', getMilestoneById)
 
 module.exports = milestoneRouter
