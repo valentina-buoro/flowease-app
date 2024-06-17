10f856519ac93ef1e2aafdb823a0b91ae63d765b
Deborah Okeke <okekedeborah@gmail.com>
2024-06-18 00:56:09 +0100

    feat: implement fetching of milestone status enum values

---
+++ controllers/milestoneControllers/getStatusEnumValuesController.js
@@ -0,0 +1,16 @@
+const MilestoneModel = require("../../models/milestoneModel");
+const { getEnumValues } = require("../../utils/getEnumValues");
+
+async function getMilestoneStatusEnumValues(req, res) {
+  try {
+      const statuses = await getEnumValues(MilestoneModel.schema, 'status')
+    //   const statuses = MilestoneModel.schema.path('status').enumValues
+
+       res.status(200).json({ success: true, message: statuses });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json("Internal server error");
+  }
+}
+
+module.exports = getMilestoneStatusEnumValues;
