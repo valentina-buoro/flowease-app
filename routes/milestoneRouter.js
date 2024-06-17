ba5089b0660949797f3bc0e4c21124491111c169
Deborah Okeke <okekedeborah@gmail.com>
2024-06-18 00:58:19 +0100

    feat: add route for fetching milestone status enum values

--- routes/milestoneRouter.js
+++ routes/milestoneRouter.js
@@ -5,6 +5,7 @@ const markMilestoneStarted = require('../controllers/milestoneControllers/milest
 const markMilestoneCompleted = require('../controllers/milestoneControllers/milestoneCompleteController')
 const getMilestoneById = require('../controllers/milestoneControllers/viewMilestoneController')
 const listMilestones = require('../controllers/milestoneControllers/listMilestonesController')
+const getMilestoneStatusEnumValues = require('../controllers/milestoneControllers/getStatusEnumValuesController')
 const milestoneRouter = express.Router()
 
 milestoneRouter.post('/:project_id/create', authenticate, createMilestone)
@@ -12,5 +13,6 @@ milestoneRouter.put('/:milestone_id/started', authenticate, markMilestoneStarted
 milestoneRouter.put('/:milestone_id/completed', authenticate, markMilestoneCompleted)
 milestoneRouter.get('/:milestone_id/milestone', getMilestoneById)
 milestoneRouter.get("/", authenticate, listMilestones); // `projectName` provided in the query string e.g '/milestones?projectName=test'
+milestoneRouter.get('/statuses', getMilestoneStatusEnumValues)
 
 module.exports = milestoneRouter
