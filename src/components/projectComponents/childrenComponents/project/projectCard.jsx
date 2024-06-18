bc9aa8daaa7be30b4d207148c6facb5a99fc4e45
valentina buoro <valentinabuoro4@gmail.com>
2024-06-18 18:08:13 +0100

    progress bar

--- src/components/projectComponents/childrenComponents/project/projectCard.jsx
+++ src/components/projectComponents/childrenComponents/project/projectCard.jsx
@@ -1,21 +1,57 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { ProgressBar } from 'react-bootstrap'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { ProgressBar } from "react-bootstrap";
 
-const ProjectCard = ({data}) => {
-  const navigate = useNavigate()
+const ProjectCard = ({ data }) => {
+  const navigate = useNavigate();
+  const completed = data.milestones.filter(
+    (milestone) => milestone.status === "Completed"
+  ).length;
+  const progress = (completed / data.milestones.length) * 100;
   return (
-    <div onClick={()=>navigate(`/projects/project-details/${data._id}`)} className='col-span-1 '>
-        <div className='border rounded-md py-4 px-6 bg-[#ffffff] hover:bg-[#E9EDFF]'>
-            <p className='font-medium text-[15px] '>{data.name}</p>
-            <p className='text-xs text-[#4E4F52]'>Task: <span className='text-primaryGreen'>20</span><span>/{data.milestones.length}</span></p>
-            <div>
-                <p className='text-[#4E4F52] text-[10px]'> Progress:</p>
-            <ProgressBar variant="success" now={40} />
-            </div>
-        </div>
-    </div>
-  )
-}
+    <div
+      onClick={() => navigate(`/projects/project-details/${data._id}`)}
+      className="col-span-1 "
+    >
+      <div className="border relative rounded-md py-4 px-6 bg-[#ffffff] hover:bg-[#E9EDFF]">
+        <p className="font-medium text-[15px] ">{data.name}</p>
+        <p className="text-xs text-[#4E4F52]">
+          Task: <span className="text-primaryGreen">{completed}</span>
+          <span>/{data.milestones.length}</span>
+        </p>
+        <div>
+          <p className="text-[#4E4F52] text-[10px]"> Progress:</p>
+          <ProgressBar
+            variant={
+              progress === 100
+                ? "success"
+                : progress > 0 && progress < 100
+                ? "warning"
+                : "transparent"
+            }
+            now={progress}
+            style={{
+              height: "5px",
+            }}
+          />
 
-export default ProjectCard
+          
+        </div>
+
+        <div className=" my-3">
+            {data.collaborators.map((src, index) => (
+              <img
+                key={index}
+                src={`https://ui-avatars.com/api/?name=${src}&background=random&color=fff`}
+                alt={`mage ${index + 1}`}
+                className="absolute bottom-1 left-10 w-[25px]  h-auto transition-transform duration-300 ease-in-out rounded-full ml-4"
+                style={{ left: `${index * 20}px` }} // Adjust the overlap offset
+              />
+            ))}
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/components/taskComponent/taskComponent.jsx
+++ src/components/taskComponent/taskComponent.jsx
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-//import Table from '../utilComponents/table';
+import Table from '../utilComponents/table';
+import { IoIosArrowRoundForward } from 'react-icons/io';
 
 const TaskComponent = () => {
    // const URL = "https://flowease.onrender.com/api";
@@ -17,20 +18,19 @@ const TaskComponent = () => {
     {header:"Task Name", accessor: "name"},
     {header:"Due Date", accessor: "due_date"},
     { header: "Assigned To", accessor: "collaborator" },
-    { header: "Status", accessor: "started" || "completed" },
+    { header: "Status", accessor: "status"  },
     {header: "Action", accessor: "action"}
    
   ];
-  console.log(columns)
 
   useEffect(() => {
     const fetchProjects = async () => {
       const token = localStorage.getItem("login_token");
-      const params = { projectName: 'pipeops' };
+     // const params = { projectName: 'pipeops' };
       setLoading(true);
       try {
         const { data } = await axios.get(
-          `https://flowease.onrender.com/api/milestones`,params,
+          `https://flowease.onrender.com/api/milestones`,
           {
             headers: {
               Authorization: `${token}`,
@@ -39,7 +39,7 @@ const TaskComponent = () => {
         );
         console.log(data);
         setProjectDetails(data.message);
-        console.log("assigned task", data.message.milestones);
+        console.log("assigned task", data.message);
         setLoading(false);
       } catch (err) {
         setLoading(false);
@@ -48,8 +48,15 @@ const TaskComponent = () => {
     fetchProjects();
   }, [id]);
   return (
-    <div>
-        hiii
+    <div className='px-4 md:px-11  w-full'>
+      <div className="flex items-center">
+          <span className="text-secondaryGrey ">Menu</span>
+
+          <IoIosArrowRoundForward className="text-[#696969]" size={30} />
+
+          <span className="text-textBlack">Projects</span>
+        </div>
+       <Table columns={columns} data={projectDetails} />
     </div>
   )
 }
