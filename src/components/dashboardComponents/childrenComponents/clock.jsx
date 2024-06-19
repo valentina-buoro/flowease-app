2fd37d7276f12dd0c0fb86208cd78e2cc2590f42
valentina buoro <valentinabuoro4@gmail.com>
2024-06-19 01:36:36 +0100

    clock in

--- src/components/dashboardComponents/childrenComponents/clock.jsx
+++ src/components/dashboardComponents/childrenComponents/clock.jsx
@@ -1,5 +1,9 @@
 import React, {useEffect, useState} from 'react'
 import WallClock from './wallclock';
+import axios from 'axios';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 const formatDateTime = (date) => {
     const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -12,6 +16,12 @@ const formatDateTime = (date) => {
 
 const DashboardClock = () => {
     const [currentDate, setCurrentDate] = useState(new Date());
+    const [loading, setLoading] = useState(false);
+    console.log(loading)
+  const [createdProjectName, setCreatedProjectName] = useState([]);
+  const [assignedProjectName, setAssignedProjectName] = useState([]);
+  const [selectedProjectName, setSelectedProjectName] = useState("");
+  
 
     // Update the date every minute (optional, if you need real-time updates)
     useEffect(() => {
@@ -23,9 +33,97 @@ const DashboardClock = () => {
       return () => clearInterval(timer);
     }, []);
 
+    useEffect(() => {
+      const fetchProjectName = async () => {
+        const token = localStorage.getItem("login_token");
+        setLoading(true);
+        try {
+          const { data } = await axios.get(
+            "https://redundant-discussion-zesty-star-production.pipeops.app/api/projects/list",
+            {
+              headers: {
+                Authorization: `${token}`,
+              },
+            }
+          );
+      
+          setAssignedProjectName(data.message.assigned_projects);
+          setCreatedProjectName(data.message.created_projects);
+          setLoading(false);
+        } catch (err) {
+          setLoading(false);
+          toast.error(err.response.data.message, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      };
+      fetchProjectName();
+    }, []);
+
+    const handleStatusChange = (event) => {
+      setSelectedProjectName(event.target.value);
+    };
+
+    const handleClockIn = async () => {
+      const token = localStorage.getItem("login_token");
+      try {
+        const { data } = await axios.post(
+          `https://redundant-discussion-zesty-star-production.pipeops.app/api/clockings/clockin`,
+          {
+            project_id: selectedProjectName,
+          },
+          {
+            headers: {
+              Authorization: `${token}`,
+            },
+          }
+        );
+        toast.success(data.message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      } catch (err) {
+        toast.error(err.response.data.message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    }
+
   return (
     <div className='col-span-1 md:col-span-2 my-12 border-[1px] border-borderGrey rounded-[10px] bg-[#FFFFFF]'>
-     <p className="font-medium text-xl text-[#1A1817] py-5 px-10"> {formatDateTime(currentDate)}</p>
+       
+     <p className="font-medium text-xl text-[#1A1817] pt-5 px-10"> {formatDateTime(currentDate)}</p>
+     <div className="mb-4 ml-4">
+        <select className="border  px-4 py-2.5 rounded bg-[#fff] text-center font-semibold" value={selectedProjectName} onChange={handleStatusChange} >
+          <option value="">Projects Name</option>
+          {assignedProjectName.map((role) => (
+            <option value={role._id} key={role._id}>{role.name}</option>
+          ))}
+          {createdProjectName.map((role) => (
+            <option value={role.name}>{role.name}</option>
+          ))}
+        </select>
+      </div>
         <div className=' flex flex-col lg:grid lg:grid-cols-6 w-full justify-between  h-[266px]'>
        <div className='col-span-1 flex flex-col justify-center items-center'>
        <p className='font-medium text-sm text-[#979797]'>Working Hours</p>
@@ -33,7 +131,7 @@ const DashboardClock = () => {
        </div>
        <div className='col-span-2'>
        <div className='flex flex-col gap-4 w-10/12 mx-auto'>
-        <button className='py-2.5 px-10 bg-primaryBlue text-[white] rounded-[10px]'>Clock In</button>
+        <button className='py-2.5 px-10 bg-primaryBlue text-[white] rounded-[10px]' onClick={handleClockIn} >Clock In</button>
         <button className='py-2.5 px-10 border border-textGrey  rounded-[10px]'>Break</button>
         <button className='py-2.5 px-10 bg-primaryBlue text-[white] rounded-[10px]'>Clock Out</button>
        </div>
